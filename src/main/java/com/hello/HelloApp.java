2dbde1868c4d22aaac68bee330cc22fc5f435d46
shwe-2703 <shwetasuresh2703@gmail.com>
2026-04-13 09:08:12 +0530

    Coded UC2:Display Hello with command line argument

--- src/main/java/com/hello/HelloApp.java
+++ src/main/java/com/hello/HelloApp.java
@@ -3,47 +3,10 @@ package com.hello;
 public class HelloApp {
 
     public static void main(String[] args) {
-
-        // Letter O
-        System.out.println(" " + "  ***  " + " ");
-        System.out.println(" " + " *   * " + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + " *   * " + " ");
-        System.out.println(" " + "  ***  " + " ");
-
-        System.out.println();
-
-        // Letter O
-        System.out.println(" " + "  ***  " + " ");
-        System.out.println(" " + " *   * " + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + " *   * " + " ");
-        System.out.println(" " + "  ***  " + " ");
-
-        System.out.println();
-
-        // Letter P
-        System.out.println(" " + "****** " + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + "*     *" + " ");
-        System.out.println(" " + "****** " + " ");
-        System.out.println(" " + "*      " + " ");
-        System.out.println(" " + "*      " + " ");
-        System.out.println(" " + "*      " + " ");
-
-        System.out.println();
-
-        // Letter S
-        System.out.println(" " + " ***** " + " ");
-        System.out.println(" " + "*      " + " ");
-        System.out.println(" " + "*      " + " ");
-        System.out.println(" " + " ***** " + " ");
-        System.out.println(" " + "      *" + " ");
-        System.out.println(" " + "      *" + " ");
-        System.out.println(" " + " ***** " + " ");
+        if (args.length > 0) {
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
     }
 }
