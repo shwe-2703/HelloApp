6c50f4364542b5416626b22477b0e6f3334e5fba
shwe-2703 <shwetasuresh2703@gmail.com>
2026-04-13 08:58:46 +0530

    Coded UC2 to Render OOPS as Banner using Print Statements

--- src/main/java/com/hello/HelloApp.java
+++ src/main/java/com/hello/HelloApp.java
@@ -1,10 +1,49 @@
 package com.hello;
 
 public class HelloApp {
-    public static void main(String[] args)
-        System.out.println("OOPS");
+
+    public static void main(String[] args) {
+
+        // Letter O
+        System.out.println(" " + "  ***  " + " ");
+        System.out.println(" " + " *   * " + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + " *   * " + " ");
+        System.out.println(" " + "  ***  " + " ");
+
+        System.out.println();
+
+        // Letter O
+        System.out.println(" " + "  ***  " + " ");
+        System.out.println(" " + " *   * " + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + " *   * " + " ");
+        System.out.println(" " + "  ***  " + " ");
+
+        System.out.println();
+
+        // Letter P
+        System.out.println(" " + "****** " + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + "*     *" + " ");
+        System.out.println(" " + "****** " + " ");
+        System.out.println(" " + "*      " + " ");
+        System.out.println(" " + "*      " + " ");
+        System.out.println(" " + "*      " + " ");
+
+        System.out.println();
+
+        // Letter S
+        System.out.println(" " + " ***** " + " ");
+        System.out.println(" " + "*      " + " ");
+        System.out.println(" " + "*      " + " ");
+        System.out.println(" " + " ***** " + " ");
+        System.out.println(" " + "      *" + " ");
+        System.out.println(" " + "      *" + " ");
+        System.out.println(" " + " ***** " + " ");
     }
-
-
-
-
+}
